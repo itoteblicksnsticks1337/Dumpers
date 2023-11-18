42acf52df7212420d1115b8727ad53045e18258d
loris <113813846+renaebaby@users.noreply.github.com>
2023-11-18 10:08:06 -0500

    Add files via upload

---
+++ Prometheus/Main.js
@@ -0,0 +1,38 @@
+const file_system = require('fs');
+const luamin = require('lua-format');
+
+const pattern = /([A-Za-z]\[[A-Za-z]\]) = [A-Za-z]\([A-Za-z]\)/g;
+
+file_system.readFile('input.lua', 'utf8', (read_error, script) => {
+  if (read_error) {
+    console.error('Error reading the file:', read_error);
+    return;
+  }
+
+  const beautified = luamin.Beautify(script, { RenameVariables: false, RenameGlobals: false, SolveMath: false });
+
+  let modified = beautified;
+  let regex_match;
+  let is_modified = false;
+
+  while ((regex_match = pattern.exec(beautified)) !== null) {
+    const matched_variable = regex_match[1];
+    modified = modified.replace(regex_match[0], `${regex_match[0]}\nprint(${matched_variable})`);
+    is_modified = true;
+    console.log(`Found and added print statement for: ${matched_variable}`);
+  }
+
+  if (!is_modified) {
+    console.log('No matches found');
+    return;
+  }
+
+  file_system.writeFile('output.lua', modified, (write_error) => {
+    if (write_error) {
+      console.error('Error writing to file:', write_error);
+      return;
+    }
+  });
+});
+
+console.log('Completed');
